9c3faa3ccc8419b5dfcded1e8b10cf5912297609
Isaac Hailperin <isaac.hailperin@gmail.com>
2016-04-28 17:22:13 +0200

    few fixes

--- auto_copy/auto_copy_daemon.py
+++ auto_copy/auto_copy_daemon.py
@@ -5,18 +5,28 @@
 The daemon that calls auto_copy.py uppon optical disc insertion
 """
 
-import signal, time
+import signal
+import sys
+import time
+
+sys.path.append('/usr/local/bin')
+
 import auto_copy
 
 SIGNAL_RECEIVED = False
 
-def run_daemon():
+def run_daemon(config):
+    """
+    Run the damon
+
+    config: configParser object
+    """
     signal.signal(signal.SIGUSR1, signal_handler)
     while True:
         time.sleep(1)
         global SIGNAL_RECEIVED
         if SIGNAL_RECEIVED:
-            auto_copy.auto_copy()
+            auto_copy.auto_copy(config)
             SIGNAL_RECEIVED = False
 
 def signal_handler(dump1, dump2):
@@ -24,6 +34,6 @@ def signal_handler(dump1, dump2):
     SIGNAL_RECEIVED = True
 
 if __name__ == "__main__":
-    config = auto_copy.read_config('/etc/auto_copy.yml')
-    auto_copy.setup_logging(config)
-    run_daemon()
+    main_config = auto_copy.read_config('/etc/auto_copy.yml')
+    auto_copy.setup_logging(main_config)
+    run_daemon(main_config)
